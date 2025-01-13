efc7d7c0bb3d55c86441af5f0281e626fad822c7
Gary Broderick <garybroderick82@gmail.com>
2025-01-13 09:58:29 +0000

    Refactor urls.py views import error handler404

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+
+handler404 = 'boutique_ado.views.handler404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
